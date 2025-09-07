9e287775653b92b04bcbf58c885e42477a91ccdb
sofiacostas <sofiacostas@riccarton.school.nz>
2025-09-08 09:19:20 +1200

    Made all the queries(not including parameter queries) seen in easygui

--- F1_driverseg.py
+++ F1_driverseg.py
@@ -2,7 +2,6 @@ import easygui as eg
 import sqlite3
 from tabulate import tabulate
 
-
 DB_NAME = 'F1_drivers.db'
 
 
@@ -11,17 +10,17 @@ TABLES = (" drivers_stats "
            "LEFT JOIN nations ON drivers_stats.nation_id = nations.nation_id ")
 
 
-def print_parameter_query(fields:str, where:str, parameter):
+def parameter_query(fields:str, where:str, parameter):
     """ Prints the results for a parameter query in tabular form. """
     db = sqlite3.connect(DB_NAME)
     cursor = db.cursor()
     sql = ("SELECT " + fields + " FROM " + TABLES + " WHERE " + where)
     cursor.execute(sql,(parameter,))
     results = cursor.fetchall()
-    print(tabulate(results,fields.split(",")))
+    eg.msgbox(tabulate(results,fields.split(",")))
     db.close()  
 
-def print_query(view_name:str):
+def query(view_name:str):
     ''' Prints the specified view from the database in a table '''
     db = sqlite3.connect('F1_drivers.db')
     cursor = db.cursor()
@@ -31,7 +30,7 @@ def print_query(view_name:str):
     field_names = "SELECT name from pragma_table_info('" + view_name + "') AS tblInfo"
     cursor.execute(field_names)
     headings = list(sum(cursor.fetchall(),()))
-    print(tabulate(results,headings))
+    eg.msgbox(tabulate(results,headings))
     db.close()
 
 
@@ -46,29 +45,55 @@ while menu_choice != 'Z':
     "- The amount of races is the amount of starts\n"
     "- Sprints are not counted in the points or races\n\n", "Notes Page")
     
-
-    ("Type the letter for the information you want: \n"
-                        "A: What team drivers are from\n"
-                        "B: What nation drivers are from\n"
-                        "C: Youngest to oldest\n"
-                        "D: Older than 25 years old\n"
-                        "E: Have 3 or more podiums\n"
-                        "F: Drivers that have more than 75 races and more than 1000 career points\n"
-                        "G: Top 8 drivers with the most points\n"
-                        "H: Have no podiums\n"
-                        "I: Drivers with at least 1 podium and 400 points\n"
-                        "J: Less than 100 races\n"
-                        "K: 100 or more races\n"
-                        "L: Less than 500 points\n"
-                        "M: In their 20s and have at least 30 races\n"
-                        "N: Between 10 to 100 podiums\n"
-                        "O: Top 5 drivers with the most podiums\n"     
-
-                        "\nZ: Exit\n\n""Type option here: " )
+    msg = "What information do you want to know?"
+    title = "Home Page"
+    choices = ["What team drivers are from\n",
+                        "What nation drivers are from\n",
+                        "Youngest to oldest\n",
+                        "Older than 25 years old\n",
+                        "Have 3 or more podiums\n",
+                        "Drivers that have more than 75 races and more than 1000 career points\n",
+                        "Top 8 drivers with the most points\n",
+                        "Have no podiums\n",
+                        "Drivers with at least 1 podium and 400 points\n",
+                        "Less than 100 races\n",
+                        "100 or more races\n",
+                        "Less than 500 points\n",
+                        "In their 20s and have at least 30 races\n",
+                        "Between 10 to 100 podiums\n",
+                        "Top 5 drivers with the most podiums\n",
+                        "EXIT"]
     
-    menu_choice = menu_choice.upper()
-    if menu_choice == 'A': 
-        print('Here are the following teams to choose from\n'
+    choice = eg.choicebox(msg, title, choices)
+    if choice == choices[2]:
+        query("All drivers youngest to oldest")
+    elif choice == choices[3]:
+        query("Older than 25")
+    elif choice == choices[4]:
+        query("3 or more podiums")
+    elif choice == choices[5]:
+        query("More than 75 races and more than 1000 points")
+    elif choice == choices[6]:
+        query("Top 8 points")
+    elif choice == choices[7]:
+        query("No podiums")
+    elif choice == choices[8]:
+        query("At least 1 podium and 400 points")    
+    elif choice == choices[9]:
+        query("Less than 100 races")
+    elif choice == choices[10]:
+        query("100 or more races")
+    elif choice == choices[1]:
+        query("Less than 500 points")
+    elif choice == choices[12]:
+        query("In 20s and at least 30 races")
+    elif choice == choices[13]:
+        query("Between 10 to 100 podiums")
+    elif choice == choices[14]:
+        query("Top 5 podiums")
+
+if choice == 'What team drivers are from': 
+        eg.msgbox('Here are the following teams to choose from\n'
               '- McLaren\n'
               '- Ferrari\n'
               '- Red Bull\n'
@@ -79,9 +104,9 @@ while menu_choice != 'Z':
               '- Haas\n'
               '- Alpine\n')
         team = input('Which team do you want to see: ').capitalize()
-        print_parameter_query("name, age, points", "team = ? ORDER BY points DESC",team)
-    elif menu_choice == 'B': 
-        print('Here are the following nations to choose from\n'
+        parameter_query("name, age, points", "team = ? ORDER BY points DESC",team)
+elif menu_choice == 'B': 
+        eg.msgbox('Here are the following nations to choose from\n'
               '- Australia\n'
               '- United Kingdom\n'
               '- Monaco\n'
@@ -97,31 +122,4 @@ while menu_choice != 'Z':
               '- Canada\n'
               '- Argentina\n')
         nation = input('Of what nation do you want to see players of: ').capitalize()
-        print_parameter_query("name, age, points", "nation = ? ORDER BY points DESC",nation)
-    elif menu_choice == 'C': 
-        print_query("All drivers youngest to oldest")
-    elif menu_choice == 'D': 
-        print_query("Older than 25")
-    elif menu_choice == 'E': 
-        print_query("3 or more podiums")
-    elif menu_choice == 'F': 
-        print_query("More than 75 races and more than 1000 points")
-    elif menu_choice == 'G': 
-        print_query("Top 8 points")
-    elif menu_choice == 'H': 
-        print_query("No podiums")
-    elif menu_choice == 'I': 
-        print_query("At least 1 podium and 400 points")    
-    elif menu_choice == 'J': 
-        print_query("Less than 100 races")
-    elif menu_choice == 'K': 
-        print_query("100 or more races")
-    elif menu_choice == 'L': 
-        print_query("Less than 500 points")
-    elif menu_choice == 'M': 
-        print_query("In 20s and at least 30 races")
-    elif menu_choice == 'N': 
-        print_query("Between 10 to 100 podiums")
-    elif menu_choice == 'O': 
-        print_query("Top 5 podiums")
-
+        parameter_query("name, age, points", "nation = ? ORDER BY points DESC",nation)
